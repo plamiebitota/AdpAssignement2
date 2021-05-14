9213ba6bbdaa85be2678634e3b69a4a846a29a6d
Plamedie Bitota <219260532@mycput.ac.za>
2021-05-14 08:18:54 +0200

    This is my first commit with class Customer and all the test except for collection.

---
+++ src/main/java/za/ac/cput/customer.java
@@ -0,0 +1,58 @@
+package za.ac.cput;
+
+import java.util.Objects;
+
+public class customer
+{
+    int customerId;
+    String  customerName;
+    String customerSurname;
+    String customerAddress;
+
+
+
+    public int getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(int customerId) {
+        this.customerId = customerId;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+    public String getCustomerSurname() {
+        return customerSurname;
+    }
+
+    public void setCustomerSurname(String customerSurname) {
+        this.customerSurname = customerSurname;
+    }
+
+    public String getCustomerAddress() {
+        return customerAddress;
+    }
+
+    public void setCustomerAddress(String customerAddress) {
+        this.customerAddress = customerAddress;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        customer customer = (customer) o;
+        return customerId == customer.customerId;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(customerId);
+    }
+}
---
+++ src/test/java/za/ac/cput/customerTest.java
@@ -0,0 +1,46 @@
+package za.ac.cput;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class customerTest
+{
+
+    @Test
+    void MapTest() {
+        Map<Integer, customer> customerMap = new HashMap<>();
+        customer c2 = new customer();
+        customerMap.put(4,c2);
+        assertEquals(c2,customerMap.get(4));
+        }
+
+
+    @Test
+    void setTest()
+    {
+        Set<customer> customerSet = new HashSet<>();
+        customer c3 = new customer();
+        customerSet.add(c3);
+        Iterator<customer> itr=customerSet.iterator();
+        while(itr.hasNext()){
+            assertSame(c3,itr.next());
+        }
+    }
+
+    @Test
+    void listTest()
+    {
+        List<customer>customerList=new ArrayList<>();
+        for(int i=0;i<5;i++)
+        {
+            customer c4= new customer();
+            customerList.add(c4);
+        }
+        assertEquals(5,customerList.size());
+
+    }
+
+}
